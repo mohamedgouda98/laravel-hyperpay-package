f73b6b03829e741a47322ae0901d079b7a1c150b
macbookpro <dev.mohamedgouda@gmail.com>
2023-04-01 15:19:39 +0200

    init

---
+++ src/HyperPayPayment.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Gouda\LaravelHyperpay;
+
+
+class HyperPayPayment
+{
+
+    public function __construct()
+    {
+        //todo
+    }
+
+    public function pay()
+    {
+        // todo
+    }
+
+
+    public function PaymentStatus()
+    {
+        // todo
+    }
+
+
+    public function updatePayment()
+    {
+        // todo
+    }
+
+
+    public function deletePayment()
+    {
+        // todo
+    }
+
+    public function getPayments()
+    {
+        // todo
+    }
+
+
+
+}
---
+++ src/HyperPayServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Gouda\LaravelHyperpay;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class HyperPayServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
+
+        $this->publishes([
+            __DIR__.'/../config/config.php' => config_path('HyperPay.php'),
+        ], 'HyperPay-package-config');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'HyperPay');
+    }
+
+}
